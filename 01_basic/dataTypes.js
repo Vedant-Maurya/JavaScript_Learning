d41776996d540d2c2519d95c23f02bf16fb704b4
Vedant-Maurya <vedant7052@gmail.com>
2025-02-08 05:15:48 -0800

    Add files via upload

---
+++ 01_basic/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code as newer version 
+
+// alert( 3 + 3 )   // we are using node js not browser
+
+
+let name ="Hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number = 2 to power 53
+// bigint = its used to very large number used then used this datatypes
+// string = "",''
+// boolean = true/false
+// null = standalone value
+// undefined
+// symbol
+
+// object
+
+console.log(typeof(undefined)) // undefined
+console.log(typeof(null))      // object
+
+
---
+++ 01_basic/variable.js
@@ -0,0 +1,18 @@
+const accountId="Rohan"
+let accountName="rohit"
+var accountNumber=12345678
+
+accountHolder="seema"
+// accountId="mohan"
+// accountName="mohit"
+
+ 
+/*
+prefer not used to var
+because issue in block scope and functional scope 
+*/
+accountName="7052"
+accountNumber="Study"
+
+console.table([accountHolder,accountId,accountName,accountNumber]);
+
